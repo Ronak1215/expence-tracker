38b7192034ff40702e63ea9b313a30d92cfc699f
Ronak1215 <ronakchitalia28@gmail.com>
2025-04-16 20:36:35 +0530

    expense provider added

--- src/context/ExpenseProvider.jsx
+++ src/context/ExpenseProvider.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
 
+export const ExpenseContext = createContext()
 const ExpenseProvider = () => {
+  const [expense, setExpense] = useState([])
+  const [title, setTitle] = useState("")
+  const [amount, setAmount] = useState("")
+  const [catagory, setCatogory] = useState("")
+  const [date, setDate] = useState("")
+
+  const addExpense = () =>{
+    if(!title || !amount || !catagory || !date){
+      alert("Pls fill all the inputs")
+    }
+  }
+
   return (
     <>
       
